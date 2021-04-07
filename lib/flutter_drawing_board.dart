7774bef0964a3b161eff6343d38d84c91ef4b1a7
光良 <guangliang.liu@pptutor.com>
2021-04-07 18:42:07 +0800

    调整细节

--- lib/flutter_drawing_board.dart
+++ lib/flutter_drawing_board.dart
@@ -1,5 +1,6 @@
 library flutter_drawing_board;
 
 export 'drawing_board.dart';
+export 'drawing_controller.dart';
 export 'helper/ex_value_builder.dart';
 export 'paint_contents/paint_content.dart';
