b3058b2f429af0afe85844e9bca1e51bcce3f34b
Mike <894659061@qq.com>
2024-03-05 11:04:16 +0800

    Rich example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,16 +12,24 @@ import 'package:flutter_drawing_board/paint_extension.dart';
 
 import 'test_data.dart';
 
+Future<ui.Image> _getImage(String path) async {
+  final Completer<ImageInfo> completer = Completer<ImageInfo>();
+  final NetworkImage img = NetworkImage(path);
+  img.resolve(ImageConfiguration.empty).addListener(
+    ImageStreamListener((ImageInfo info, _) {
+      completer.complete(info);
+    }),
+  );
+
+  final ImageInfo imageInfo = await completer.future;
+
+  return imageInfo.image;
+}
+
 const Map<String, dynamic> _testLine1 = <String, dynamic>{
   'type': 'StraightLine',
-  'startPoint': <String, dynamic>{
-    'dx': 68.94337550070736,
-    'dy': 62.05980083656557
-  },
-  'endPoint': <String, dynamic>{
-    'dx': 277.1373386828114,
-    'dy': 277.32029957032194
-  },
+  'startPoint': <String, dynamic>{'dx': 68.94337550070736, 'dy': 62.05980083656557},
+  'endPoint': <String, dynamic>{'dx': 277.1373386828114, 'dy': 277.32029957032194},
   'paint': <String, dynamic>{
     'blendMode': 3,
     'color': 4294198070,
@@ -35,14 +45,8 @@ const Map<String, dynamic> _testLine1 = <String, dynamic>{
 
 const Map<String, dynamic> _testLine2 = <String, dynamic>{
   'type': 'StraightLine',
-  'startPoint': <String, dynamic>{
-    'dx': 106.35164817830423,
-    'dy': 255.9575653134524
-  },
-  'endPoint': <String, dynamic>{
-    'dx': 292.76034659254094,
-    'dy': 92.125586665872
-  },
+  'startPoint': <String, dynamic>{'dx': 106.35164817830423, 'dy': 255.9575653134524},
+  'endPoint': <String, dynamic>{'dx': 292.76034659254094, 'dy': 92.125586665872},
   'paint': <String, dynamic>{
     'blendMode': 3,
     'color': 4294198070,
@@ -56,7 +60,7 @@ const Map<String, dynamic> _testLine2 = <String, dynamic>{
   }
 };
 
-/// 自定义绘制三角形
+/// Custom drawn triangles
 class Triangle extends PaintContent {
   Triangle();
 
@@ -89,8 +93,7 @@ class Triangle extends PaintContent {
 
   @override
   void drawing(Offset nowPoint) {
-    A = Offset(
-        startPoint.dx + (nowPoint.dx - startPoint.dx) / 2, startPoint.dy);
+    A = Offset(startPoint.dx + (nowPoint.dx - startPoint.dx) / 2, startPoint.dy);
     B = Offset(startPoint.dx, nowPoint.dy);
     C = nowPoint;
   }
@@ -116,6 +119,62 @@ class Triangle extends PaintContent {
       'A': A.toJson(),
       'B': B.toJson(),
       'C': C.toJson(),
+      'paint': paint.toJson(),
+    };
+  }
+}
+
+/// Custom drawn image
+/// url: https://web-strapi.mrmilu.com/uploads/flutter_logo_470e9f7491.png
+const String _imageUrl = 'https://web-strapi.mrmilu.com/uploads/flutter_logo_470e9f7491.png';
+
+class ImageContent extends PaintContent {
+  ImageContent(this.image, {this.imageUrl = ''});
+
+  ImageContent.data({
+    required this.startPoint,
+    required this.size,
+    required this.image,
+    required this.imageUrl,
+    required Paint paint,
+  }) : super.paint(paint);
+
+  factory ImageContent.fromJson(Map<String, dynamic> data) {
+    return ImageContent.data(
+      startPoint: jsonToOffset(data['startPoint'] as Map<String, dynamic>),
+      size: jsonToOffset(data['size'] as Map<String, dynamic>),
+      imageUrl: data['imageUrl'] as String,
+      image: data['image'] as ui.Image,
+      paint: jsonToPaint(data['paint'] as Map<String, dynamic>),
+    );
+  }
+
+  Offset startPoint = Offset.zero;
+  Offset size = Offset.zero;
+  final String imageUrl;
+  final ui.Image image;
+
+  @override
+  void startDraw(Offset startPoint) => this.startPoint = startPoint;
+
+  @override
+  void drawing(Offset nowPoint) => size = nowPoint - startPoint;
+
+  @override
+  void draw(Canvas canvas, Size size, bool deeper) {
+    final Rect rect = Rect.fromPoints(startPoint, startPoint + this.size);
+    paintImage(canvas: canvas, rect: rect, image: image, fit: BoxFit.fill);
+  }
+
+  @override
+  ImageContent copy() => ImageContent(image);
+
+  @override
+  Map<String, dynamic> toContentJson() {
+    return <String, dynamic>{
+      'startPoint': startPoint.toJson(),
+      'size': size.toJson(),
+      'imageUrl': imageUrl,
       'paint': paint.toJson(),
     };
   }
@@ -156,8 +215,7 @@ class _MyHomePageState extends State<MyHomePage> {
   /// 绘制控制器
   final DrawingController _drawingController = DrawingController();
 
-  final TransformationController _transformationController =
-      TransformationController();
+  final TransformationController _transformationController = TransformationController();
 
   @override
   void dispose() {
@@ -167,8 +225,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   /// 获取画板数据 `getImageData()`
   Future<void> _getImageData() async {
-    final Uint8List? data =
-        (await _drawingController.getImageData())?.buffer.asUint8List();
+    final Uint8List? data = (await _drawingController.getImageData())?.buffer.asUint8List();
     if (data == null) {
       debugPrint('获取图片数据失败');
       return;
@@ -180,8 +237,7 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (BuildContext c) {
           return Material(
             color: Colors.transparent,
-            child: InkWell(
-                onTap: () => Navigator.pop(c), child: Image.memory(data)),
+            child: InkWell(onTap: () => Navigator.pop(c), child: Image.memory(data)),
           );
         },
       );
@@ -201,12 +257,10 @@ class _MyHomePageState extends State<MyHomePage> {
             child: InkWell(
               onTap: () => Navigator.pop(c),
               child: Container(
-                constraints:
-                    const BoxConstraints(maxWidth: 500, maxHeight: 800),
+                constraints: const BoxConstraints(maxWidth: 500, maxHeight: 800),
                 padding: const EdgeInsets.all(20.0),
                 child: SelectableText(
-                  const JsonEncoder.withIndent('  ')
-                      .convert(_drawingController.getJsonList()),
+                  const JsonEncoder.withIndent('  ').convert(_drawingController.getJsonList()),
                 ),
               ),
             ),
@@ -219,8 +273,7 @@ class _MyHomePageState extends State<MyHomePage> {
   /// 添加Json测试内容
   void _addTestLine() {
     _drawingController.addContent(StraightLine.fromJson(_testLine1));
-    _drawingController
-        .addContents(<PaintContent>[StraightLine.fromJson(_testLine2)]);
+    _drawingController.addContents(<PaintContent>[StraightLine.fromJson(_testLine2)]);
     _drawingController.addContent(SimpleLine.fromJson(tData[0]));
     _drawingController.addContent(Eraser.fromJson(tData[1]));
   }
@@ -238,14 +291,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Drawing Test'),
         systemOverlayStyle: SystemUiOverlayStyle.light,
         actions: <Widget>[
-          IconButton(
-              icon: const Icon(Icons.line_axis), onPressed: _addTestLine),
-          IconButton(
-              icon: const Icon(Icons.javascript_outlined), onPressed: _getJson),
+          IconButton(icon: const Icon(Icons.line_axis), onPressed: _addTestLine),
+          IconButton(icon: const Icon(Icons.javascript_outlined), onPressed: _getJson),
           IconButton(icon: const Icon(Icons.check), onPressed: _getImageData),
-          IconButton(
-              icon: const Icon(Icons.restore_page_rounded),
-              onPressed: _restBoard),
+          IconButton(icon: const Icon(Icons.restore_page_rounded), onPressed: _restBoard),
         ],
       ),
       body: Column(
@@ -272,8 +321,38 @@ class _MyHomePageState extends State<MyHomePage> {
                         DefToolItem(
                           icon: Icons.change_history_rounded,
                           isActive: t == Triangle,
-                          onTap: () =>
-                              _drawingController.setPaintContent(Triangle()),
+                          onTap: () => _drawingController.setPaintContent(Triangle()),
+                        ),
+                      )
+                      ..insert(
+                        2,
+                        DefToolItem(
+                          icon: Icons.image_rounded,
+                          isActive: t == ImageContent,
+                          onTap: () async {
+                            showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext c) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              },
+                            );
+
+                            try {
+                              _drawingController.setPaintContent(ImageContent(
+                                await _getImage(_imageUrl),
+                                imageUrl: _imageUrl,
+                              ));
+                            } catch (e) {
+                              //
+                            } finally {
+                              if (context.mounted) {
+                                Navigator.pop(context);
+                              }
+                            }
+                          },
                         ),
                       );
                   },
